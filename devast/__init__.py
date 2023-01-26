b1a9d419f70c3bf5b60f556ef7f9b91ca0e3b449
lnlyct <lnlyct@proton.me>
2023-01-26 12:20:20 -0800

    Working logging; snapshot commit in case i forget how i got here

--- devast/__init__.py
+++ devast/__init__.py
@@ -7,9 +7,28 @@ __license__ = "See LICENSE for more details."
 __version__ = "0.0.0"
 
 from .common import database, logger, parser
-from multiprocessing import Queue
+from multiprocessing import Queue, Process
 import logging
 
+import random
+import time
+
+i = 0
+
+def worker_process(q):
+    qh = logging.handlers.QueueHandler(q)
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    root.addHandler(qh)
+    levels = [logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR,
+              logging.CRITICAL]
+    loggers = ['foo', 'foo.bar', 'foo.bar.baz',
+               'spam', 'spam.ham', 'spam.ham.eggs']
+    for i in range(100):
+        lvl = random.choice(levels)
+        logger = logging.getLogger(random.choice(loggers))
+        logger.log(lvl, 'Message no. %d', i)
+        time.sleep(1)
 
 def run():
     """
@@ -18,9 +37,19 @@ def run():
     TODO: Run as daemon with interactive shell.
     """
     log_queue = Queue()
+
+    # qh = logging.handlers.QueueHandler(log_queue)
+    # root = logging.getLogger()
+    # # root.setLevel(logging.DEBUG)
+    # root.addHandler(qh)
+
     log_listener = logger.Listener(group=None, queue=log_queue)
+    wp = Process(target=worker_process, name='worker %d' % (i + 1), args=(log_queue,))
+    wp.start()
     log_listener.start()
     # Put all runtime logic in between the logger thread
+
+    wp.join()
 
     log_listener.join()
 

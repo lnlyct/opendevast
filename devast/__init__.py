7e9dd4011993b9e04ee7307f8b740a3c7fa5757c
lnlyct <lnlyct@proton.me>
2023-01-26 11:26:25 -0800

    Work on logger a bit; more of a commit to ssave a working version before hell insues :)

--- devast/__init__.py
+++ devast/__init__.py
@@ -1,7 +1,7 @@
 # Following PEP 681
 __title__ = "Devastation"
 __author__ = "LonelyCat"
-__description__ = "Open implementation of devast.io"
+__description__ = "Open implementation of Devastation."
 __copyright__ = "(c) 2017-present LapaMauve"
 __license__ = "See LICENSE for more details."
 __version__ = "0.0.0"
@@ -17,5 +17,7 @@ def run():
     TODO: Run as daemon with interactive shell.
     """
     log_queue = Queue()
-
+    log = logger.Logger(group=None, log_queue=log_queue)
+    log.start()
+    log.join()
 
--- devast/common/logger/logger.py
+++ devast/common/logger/logger.py
@@ -13,13 +13,33 @@ class Logger(Thread):
     logs all events to the console and log files.
     """
 
-    def __init__(self, log_queue) -> None:
+    def __init__(self, group, log_queue) -> None:
+        """
+        Params:
+        group (NoneType, required) should be None; reserved for future extension when a ThreadGroup class is implemented
+        log_queue (Queue(), required) shared queue the logger will listen to for new logs.
+
+        Returns:
+            None
+        """
         # self.handler = logging.getStreamHandler(
             
+        # # )
+        # self.formatter = logging.Formatter(
+        #     fmt="%(asctime)s %(name)s %(levelname)s %(message)s",
+        #     datefmt="[%Y-%m-%d %H:%M:%S]"
         # )
-        self.formatter = logging.getFormatter(
-            fmt="%(asctime)s %(name)s %(levelname)s %(message)s",
-            datefmt="[%Y-%m-%d %H:%M:%S]"
-        )
+        self.log_queue = log_queue
 
-        Thread.__init__(self, log_queue)
+        Thread.__init__(self, group, log_queue) # log_queue may not be needed here, find out by testing!
+
+    def run(self):
+        """
+        TODO: Add description of run() here!
+        """
+        while True:
+            record = self.log_queue.get()
+            if record is None:
+                break # Exits if a NoneType is sent to the queue
+            logger = logging.getLogger(record.name)
+            logger.handle(record)
